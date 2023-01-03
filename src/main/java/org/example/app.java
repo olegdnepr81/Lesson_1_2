e240d31dd9eabce6e46e89e3db5474ead9695de5
olegpysarenko <olegitdnepr@gmail.com>
2023-01-03 14:39:24 +0200

    Initial commit

---
+++ src/main/java/org/example/app.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class app {
+    public static void main(String[] args) throws JsonProcessingException {
+
+        ObjectMapper objectsMapper = new ObjectMapper();
+        System.out.println(objectsMapper.writeValueAsString(new Person("Oleh", "Pysarenko")));
+    }
+}
